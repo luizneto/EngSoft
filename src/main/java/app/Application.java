fa903ffd2a4a9549b65a9cdd80f7eea05dc52d3b
luizneto <luizesantosneto@gmail.com>
2019-03-14 08:50:36 -0300

    Commit de atualização do Application boot
    Adicionando o Timerlog para informa os logs no spring boot;

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -15,8 +15,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
-    }
+        TimerLog timerLog = new TimerLog();
+		timerLog.log();
+	}
 
 }
