21cbd35ee030da3a596370563d14b38ee6b19827
luizneto <luizesantosneto@gmail.com>
2019-03-14 08:42:54 -0300

    Commit de atualização da Controller Voo
    Adicionado tratamento de exceção caso seja inserido um icao24 duplicado.

--- src/main/java/app/VooController.java
+++ src/main/java/app/VooController.java
@@ -7,19 +7,23 @@ import java.util.Map;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 
+import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import dao.VooDao;
-
 @Controller
 public class VooController {
 
 	@Autowired
 	private VooDao repositorio;
-	
+
+	private static final Logger logger = (Logger) LoggerFactory.getLogger(VooController.class);
+
 	@GetMapping("/")
     public String index(Model model) {
         atualizarBase();
@@ -56,10 +60,15 @@ public class VooController {
 	    final List<Voo> voos = new ArrayList<Voo>();
 	    final List lines = (List)map.get("states");
 	    for(int i = 0, l = lines.size(); i < l; i++) {
-	      final List line = (List)lines.get(i);
-	      voos.add(new Voo(line));
+	      	try {
+		    	final List line = (List)lines.get(i);
+	      		voos.add(new Voo(line));
+          		repositorio.saveAll(voos);	      		
+	     	}catch(Exception ex) {
+	    		logger.warn("Warn: Esse icao24 já consta na base: " + ex);
+	    		logger.error("Error: Esse icao24 já consta na base: " + ex);
+	      	}
 	    }
-	    repositorio.saveAll(voos);
+	    
 	  }
-
 }
